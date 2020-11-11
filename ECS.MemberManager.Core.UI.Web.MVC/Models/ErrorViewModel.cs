9bbb699437ff8d95e6b82b1371a3a2cdabd8adf4
Ed Mathias <edmathias18@gmail.com>
2020-11-11 15:47:59 -0700

    Add MemberManager Razor and MVC projects

---
+++ ECS.MemberManager.Core.UI.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECS.MemberManager.Core.UI.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
