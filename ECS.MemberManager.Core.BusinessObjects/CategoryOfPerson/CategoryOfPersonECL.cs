c25850f4949d92cac9d79bf18e871256b6be2529
Ed Mathias <edmathias18@gmail.com>
2020-12-11 18:22:18 -0700

    Add CategoryOfPersonECL.cs

---
+++ ECS.MemberManager.Core.BusinessObjects/CategoryOfPerson/CategoryOfPersonECL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Csla;
+using ECS.MemberManager.Core.EF.Domain;
+
+namespace ECS.MemberManager.Core.BusinessObjects
+{
+    [Serializable]
+    public class CategoryOfPersonECL : BusinessListBase<CategoryOfPersonECL, CategoryOfPersonEC>
+    {
+        
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void AddObjectAuthorizationRules()
+        {
+            // TODO: add object-level authorization rules
+        }
+
+        internal static async Task<CategoryOfPersonECL> NewCategoryOfPersonList()
+        {
+            return await DataPortal.CreateChildAsync<CategoryOfPersonECL>();
+        }
+
+        internal static async Task<CategoryOfPersonECL> GetCategoryOfPersonList(IList<CategoryOfPerson> listOfChildren)
+        {
+            return await DataPortal.FetchChildAsync<CategoryOfPersonECL>(listOfChildren);
+        }
+
+        [FetchChild]
+        private async void FetchChild(IList<CategoryOfPerson> listOfChildren)
+        {
+            RaiseListChangedEvents = false;
+            foreach (var categoryOfPerson in listOfChildren)
+            {
+                this.Add(await CategoryOfPersonEC.GetCategoryOfPerson(categoryOfPerson));
+            }
+
+            RaiseListChangedEvents = true;
+        }
+            
+    }
+}
