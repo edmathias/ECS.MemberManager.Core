a87ec9a546545533051be04320d1f7c9a46b528b
Ed Mathias <edmathias18@gmail.com>
2021-01-14 11:43:38 -0700

    finish ContactForSponsor business objects and tests

---
+++ ECS.MemberManager.Core.BusinessObjects.xUnitTest/ContactForSponsor/ContactForSponsorInfo_Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Csla;
+using Csla.Rules;
+using ECS.MemberManager.Core.DataAccess.ADO;
+using ECS.MemberManager.Core.DataAccess.Mock;
+using ECS.MemberManager.Core.EF.Domain;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace ECS.MemberManager.Core.BusinessObjects.xUnitTest
+{
+    public class ContactForSponsorInfo_Tests
+    {
+        private IConfigurationRoot _config = null;
+        private bool IsDatabaseBuilt = false;
+
+        public ContactForSponsorInfo_Tests()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _config = builder.Build();
+            var testLibrary = _config.GetValue<string>("TestLibrary");
+
+            if (testLibrary == "Mock")
+                MockDb.ResetMockDb();
+            else
+            {
+                if (!IsDatabaseBuilt)
+                {
+                    var adoDb = new ADODb();
+                    adoDb.BuildMemberManagerADODb();
+                    IsDatabaseBuilt = true;
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ContactForSponsorInfo_TestGetContactForSponsorInfo()
+        {
+            const int ID_TO_FETCH = 1;
+            
+            var contactForSponsor = await ContactForSponsorInfo.GetContactForSponsorInfo(ID_TO_FETCH);
+
+            Assert.NotNull(contactForSponsor);
+            Assert.Equal(ID_TO_FETCH,contactForSponsor.Id);
+        }
+  
+    }
+}
