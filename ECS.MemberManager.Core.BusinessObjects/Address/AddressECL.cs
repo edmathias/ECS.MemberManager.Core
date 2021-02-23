82a8cd06e8373dde3b6431e017aa5bebdd3c1cdd
Ed Mathias <edmathias18@gmail.com>
2021-02-23 09:02:58 -0700

    Address T4 and tests passed

---
+++ ECS.MemberManager.Core.BusinessObjects/Address/AddressECL.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+using System; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Csla;
+using ECS.MemberManager.Core.DataAccess;
+using ECS.MemberManager.Core.DataAccess.Dal;
+using ECS.MemberManager.Core.EF.Domain;
+
+namespace ECS.MemberManager.Core.BusinessObjects
+{
+    [Serializable]
+    public partial class AddressECL : BusinessListBase<AddressECL,AddressEC>
+    {
+        #region Factory Methods
+
+        internal static async Task<AddressECL> NewAddressECL()
+        {
+            return await DataPortal.CreateChildAsync<AddressECL>();
+        }
+
+        internal static async Task<AddressECL> GetAddressECL(List<Address> childData)
+        {
+            return await DataPortal.FetchChildAsync<AddressECL>(childData);
+        }
+
+        #endregion
+
+        #region Data Access
+ 
+        [FetchChild]
+        private async Task Fetch(List<Address> childData)
+        {
+
+            using (LoadListMode)
+            {
+                foreach (var domainObjToAdd in childData)
+                {
+                    var objectToAdd = await AddressEC.GetAddressEC(domainObjToAdd);
+                    Add(objectToAdd);
+                }
+            }
+        }
+       
+        [Update]
+        private void Update()
+        {
+            Child_Update();
+        }
+
+        #endregion
+
+     }
+}
---
+++ ECS.MemberManager.Core.BusinessObjects/Organization/OrganizationECL.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+using System; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Csla;
+using ECS.MemberManager.Core.DataAccess;
+using ECS.MemberManager.Core.DataAccess.Dal;
+using ECS.MemberManager.Core.EF.Domain;
+
+namespace ECS.MemberManager.Core.BusinessObjects
+{
+    [Serializable]
+    public partial class OrganizationECL : BusinessListBase<OrganizationECL,OrganizationEC>
+    {
+        #region Factory Methods
+
+        internal static async Task<OrganizationECL> NewOrganizationECL()
+        {
+            return await DataPortal.CreateChildAsync<OrganizationECL>();
+        }
+
+        internal static async Task<OrganizationECL> GetOrganizationECL(List<Organization> childData)
+        {
+            return await DataPortal.FetchChildAsync<OrganizationECL>(childData);
+        }
+
+        #endregion
+
+        #region Data Access
+ 
+        [FetchChild]
+        private async Task Fetch(List<Organization> childData)
+        {
+
+            using (LoadListMode)
+            {
+                foreach (var domainObjToAdd in childData)
+                {
+                    var objectToAdd = await OrganizationEC.GetOrganizationEC(domainObjToAdd);
+                    Add(objectToAdd);
+                }
+            }
+        }
+
+        #endregion
+
+     }
+}
