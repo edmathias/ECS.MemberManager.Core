06c0f9025c0ffbb02e9516d2ee7c328c6e43189a
Ed Mathias <edmathias18@gmail.com>
2021-03-16 10:54:23 -0600

    Image data-first updates

--- ECS.MemberManager.Core.EF.Domain/Image.cs
+++ ECS.MemberManager.Core.EF.Domain/Image.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Net.Mime;
 
 namespace ECS.MemberManager.Core.EF.Domain
 {
     public class Image : EntityBase
     {
-        public string ImagePath;
+        public string ImagePath { get; set; }
 
-        public byte[] ImageFile;
+        public byte[] ImageFile { get; set; }
     }
 }
